d6125bbf09c0efe4c29f30af1cb19e4965c3917b
JehovannyMaradiaga <maradiagawalter6@gmail.com>
2025-04-04 11:18:52 -0600

    Actualizacion de la version del proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RRHH_TestAplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
